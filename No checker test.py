d61b7a9651b07a7a5a2ea8faca69baf6062b0838
TheMale1 <98923416+TheMale1@users.noreply.github.com>
2022-03-25 12:01:21 +1300

    Update No checker test.py

--- No checker test.py
+++ No checker test.py
@@ -1,5 +1,5 @@
 # Ask the user if they have played the game before
-show_instructions = input("Have you played this game before (Yes/No): ")
+show_instructions = input("Have you played this game before (Yes/No) : ")
 # If they say yes, output 'Program continues'
 if show_instructions == "Yes":
     print("Program continues")
